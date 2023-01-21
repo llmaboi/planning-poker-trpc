da1853f9f08d5eb1a27e20ff2febc19c2174f65e
Sam Peterson <samllama55@gmail.com>
2023-01-21 11:38:29 +0100

    CHANGE: Split env prod and dev server startup.

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,7 +1,4 @@
-import * as dotenv from 'dotenv';
 import { z } from 'zod';
-
-dotenv.config();
 
 const parsedEnvRaw = z.object({
   VITE_APP_PORT: z.string(),
@@ -31,14 +28,46 @@ const parsedEnv = z.object({
   VITE_DEV: z.boolean(),
 });
 
-const envRaw = parsedEnvRaw.parse(process.env);
+const parsedProdEnvRaw = z.object({
+  VITE_API_PREFIX: z.string(),
+  VITE_API_PORT: z.string(),
+
+  VITE_MYSQL_HOST: z.string(),
+  VITE_MYSQL_NAME: z.string(),
+  VITE_MYSQL_USER: z.string(),
+  VITE_MYSQL_PASSWORD: z.string(),
+});
+
+const parsedProdEnv = z.object({
+  VITE_API_PREFIX: z.string(),
+  VITE_API_PORT: z.number(),
+
+  VITE_MYSQL_HOST: z.string(),
+  VITE_MYSQL_NAME: z.string(),
+  VITE_MYSQL_USER: z.string(),
+  VITE_MYSQL_PASSWORD: z.string(),
+});
 
 export type ParsedEnv = z.infer<typeof parsedEnv>;
+export type ParsedProdEnv = z.infer<typeof parsedProdEnv>;
 
-export const serverConfig: ParsedEnv = {
-  ...envRaw,
-  VITE_APP_PORT: parseInt(envRaw.VITE_APP_PORT),
-  VITE_MYSQL_PORT: parseInt(envRaw.VITE_MYSQL_PORT),
-  VITE_API_PORT: parseInt(envRaw.VITE_API_PORT),
-  VITE_DEV: envRaw.VITE_DEV && envRaw.VITE_DEV === 'true' ? true : false,
-};
+export function getProdServerConfig(): ParsedProdEnv {
+  const envRaw = parsedProdEnvRaw.parse(process.env);
+
+  return {
+    ...envRaw,
+    VITE_API_PORT: parseInt(envRaw.VITE_API_PORT),
+  };
+}
+
+export function getDevServerConfig(): ParsedEnv {
+  const envRaw = parsedEnvRaw.parse(process.env);
+
+  return {
+    ...envRaw,
+    VITE_APP_PORT: parseInt(envRaw.VITE_APP_PORT),
+    VITE_MYSQL_PORT: parseInt(envRaw.VITE_MYSQL_PORT),
+    VITE_API_PORT: parseInt(envRaw.VITE_API_PORT),
+    VITE_DEV: envRaw.VITE_DEV && envRaw.VITE_DEV === 'true' ? true : false,
+  };
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,9 +2,9 @@ import fastifyMysql, { MySQLPromisePool } from '@fastify/mysql';
 import ws from '@fastify/websocket';
 import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
 import fastify from 'fastify';
-import { appRouter } from './router/index.js';
+import { ParsedEnv, ParsedProdEnv } from './config.js';
 import { createContext } from './router/context.js';
-import { ParsedEnv } from './config.js';
+import { appRouter } from './router/index.js';
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -26,7 +26,55 @@ const envToLogger = {
   test: false,
 };
 
-export function createServer(opts: ParsedEnv) {
+export function createProdServer({
+  VITE_MYSQL_HOST,
+  VITE_MYSQL_NAME,
+  VITE_MYSQL_PASSWORD,
+  VITE_MYSQL_USER,
+  VITE_API_PREFIX,
+  VITE_API_PORT,
+}: ParsedProdEnv) {
+  const logger = envToLogger['production'];
+  const server = fastify({ logger });
+
+  void server.register(ws);
+
+  void server.register(fastifyMysql, {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+    database: VITE_MYSQL_NAME,
+    host: VITE_MYSQL_HOST,
+
+    user: VITE_MYSQL_USER,
+    password: VITE_MYSQL_PASSWORD,
+    promise: true,
+  });
+
+  server.get('/', async () => {
+    return Promise.resolve({ hello: 'wait-on 💨' });
+  });
+
+  void server.register(fastifyTRPCPlugin, {
+    prefix: VITE_API_PREFIX,
+    useWSS: true,
+    trpcOptions: { router: appRouter, createContext },
+  });
+
+  const stop = () => server.close();
+  const start = async () => {
+    try {
+      await server.listen({ port: VITE_API_PORT });
+    } catch (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+  };
+
+  return { server, start, stop };
+}
+
+export function createDevServer(opts: ParsedEnv) {
   const dev = opts.VITE_DEV;
   const port = opts.VITE_API_PORT;
   const prefix = opts.VITE_API_PREFIX;
